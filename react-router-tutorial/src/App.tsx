b04415b508590961cbdb4900e51a8fabc1400fd8
Charles Borges <solucao.erp@gmail.com>
2023-09-25 22:28:16 -0300

    react-router: Links without full page reload

--- react-router-tutorial/src/App.tsx
+++ react-router-tutorial/src/App.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import './App.css'
 
 export default function App() {
   return (
     <div>
       <h1>Bookkeeper!</h1>
+      <nav className="router-links">
+        <Link to="/invoices">Invoices</Link> | {" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
