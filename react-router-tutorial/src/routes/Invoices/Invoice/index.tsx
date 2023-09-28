345b9c0cfc9e7839884cd0dae25e3dfbcd18163b
Charles Borges <solucao.erp@gmail.com>
2023-09-27 21:47:04 -0300

    react-router-tutorial: Navigating Programmatically

--- react-router-tutorial/src/routes/Invoices/Invoice/index.tsx
+++ react-router-tutorial/src/routes/Invoices/Invoice/index.tsx
@@ -1,10 +1,11 @@
 import './styles.css';
 
-import { useParams } from "react-router-dom";
-import { getInvoice } from "../../../data/data";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteInvoice, getInvoice } from "../../../data/data";
 
 export default function Invoice() {
     const params = useParams();
+    const navigate = useNavigate();
 
     if (!params.invoiceId) {
         return <div>Invoice ID not found!</div>;
@@ -12,13 +13,27 @@ export default function Invoice() {
 
     const invoice = getInvoice(Number(params.invoiceId));
 
+    if (!invoice) {
+        return <div>Invoice not found!</div>;
+    }
+
     return (
         <main className="invoice-main">
-            <h2 className="invoice-header">Total Due: {invoice?.amount}</h2>
+            <h2 className="invoice-header">Total Due: {invoice.amount}</h2>
             <p className="invoice-details">
-                {invoice?.name}: {invoice?.number}
+                {invoice.name}: {invoice.number}
             </p>
-            <p className="invoice-details">Due Date: {invoice?.due}</p>
+            <p className="invoice-details">Due Date: {invoice.due}</p>
+            <p>
+                <button
+                    onClick={() => {
+                        deleteInvoice(invoice.number);
+                        navigate("/invoices" + location.search);
+                    }}
+                >
+                    Delete
+                </button>
+            </p>
         </main>
     );
 }
