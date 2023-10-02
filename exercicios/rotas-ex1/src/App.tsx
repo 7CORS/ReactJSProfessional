de2611cbe1545f7ff025f2127e3b3e9732c6dfbc
Charles Borges <solucao.erp@gmail.com>
2023-10-02 19:41:58 -0300

    rotas-ex1: Estrutura básica de rotas

--- exercicios/rotas-ex1/src/App.tsx
+++ exercicios/rotas-ex1/src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+import Promotion from "./routes/Home/Promotion";
+
 export default function App() {
   return (
-    <>
-      <h1>Olá Mundo!</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} >
+          <Route index element={<HomeBody />} />
+          <Route path="promotion" element={<Promotion />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
