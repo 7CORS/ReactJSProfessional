04a08568442e21b83a43937716019ed2498117d4
Charles Borges <solucao.erp@gmail.com>
2023-10-02 21:29:36 -0300

    rotas-ex2: Finalização do exercício

--- exercicios/rotas-ex2/src/App.tsx
+++ exercicios/rotas-ex2/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import Promotion from "./routes/Home/Promotion";
-import Subscription from "./routes/Home/Subscription";
+import Subscription from "./routes/Subscription";
 
 export default function App() {
   return (
@@ -13,8 +13,9 @@ export default function App() {
         <Route path="/" element={<Home />} >
           <Route index element={<HomeBody />} />
           <Route path="promotion" element={<Promotion />} />
-          <Route path="sub" element={<Subscription />} />
         </Route>
+
+        <Route path="/sub" element={<Subscription />} />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
