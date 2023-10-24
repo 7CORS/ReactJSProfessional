5a9a4fa222930d85c0738e7655bc4265f8b5ef7f
Charles Borges <solucao.erp@gmail.com>
2023-10-23 22:15:55 -0300

    Frontend-Web: Decrementar a quantidade de um Item do carrinho

--- dscommerce-frontend/src/services/cart-service.ts
+++ dscommerce-frontend/src/services/cart-service.ts
@@ -34,3 +34,26 @@ export function increaseItem(productId: number) {
         cartRepository.save(cart);
     }
 }
+
+export function decreaseItem(productId: number) {
+    // 1. Recupera o carrinho atual
+    const cart = cartRepository.get();
+
+    // 2. Tenta encontrar o item no carrinho pelo ID do produto
+    const item = cart.items.find(x => x.productId === productId);
+
+    // 3. Se o item foi encontrado...
+    if (item) {
+        // 3.1. Diminui a quantidade do item
+        item.quantity--;
+
+        // 3.2. Se a quantidade do item for menor que 1 após a diminuição...
+        if (item.quantity < 1) {
+            // 3.2.1. Remove o item do carrinho
+            cart.items = cart.items.filter(x => x.productId !== productId);
+        }
+
+        // 4. Salva as alterações no repositório do carrinho
+        cartRepository.save(cart);
+    }
+}
