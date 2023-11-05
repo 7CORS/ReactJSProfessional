cd01efefcf602d6be9f218793856f20670cd6058
Charles Borges <solucao.erp@gmail.com>
2023-11-05 17:23:11 -0300

    form-numero: Tratamento de valores mínimos e máximos enviados

--- form-numero/src/App.tsx
+++ form-numero/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import { ChangeEventType } from "./utils/TypesEvents";
+import { ChangeEventType, FormEventType } from "./utils/TypesEvents";
 
 type FormData = {
   salary?: number;
+  minValue?: number;
+  maxValue?: number;
 }
 
 export default function App() {
@@ -15,13 +17,36 @@ export default function App() {
     setFormData({ ...formData, [name]: value })
   }
 
+  function handleSubmit(event: FormEventType) {
+    event.preventDefault();
+
+    // Tratando os valores enviados
+    console.log("Salário: ", formData.salary || 0);
+    console.log("Valor mínimo: ", formData.minValue || 0);
+    console.log("Valor máximo: ", formData.maxValue || Number.MAX_VALUE);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <input
+        name="minValue"
+        value={formData.minValue || ""}
+        type="text"
+        placeholder="Digite o valor mínimo"
+        onChange={handleInputChange}
+      />
+      <input
+        name="maxValue"
+        value={formData.maxValue || ""}
+        type="text"
+        placeholder="Digite o valor máximo"
+        onChange={handleInputChange}
+      />
       <input
         name="salary"
         value={formData.salary || ""}
         type="text"
-        placeholder="Digite um número"
+        placeholder="Digite o salário"
         onChange={handleInputChange}
       />
       <button type="submit">Enviar</button>
