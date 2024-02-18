b7bce81eb790046e03998d9a9099b846c42b14c9
Charles Borges <solucao.erp@gmail.com>
2024-02-18 16:19:33 -0300

    Frontend-Web: Salvar token globalmente no localStorage

--- dscommerce-frontend/src/utils/System/index.ts
+++ dscommerce-frontend/src/utils/System/index.ts
@@ -1,8 +1,9 @@
 // URL base do backend
 export const BASE_URL = "http://localhost:8080";
 
-// Chave para o armazenamento do carrinho no localStorage
+// Chave para o armazenamento do carrinho e token no localStorage
 export const CART_KEY = "com.devsuperior.dscommerce/Cart";
+export const TOKEN_KEY = "com.devsuperior.dscommerce/Token";
 
 // ID do cliente para autenticação OAuth2
 export const CLIENT_ID = "myclientid";
