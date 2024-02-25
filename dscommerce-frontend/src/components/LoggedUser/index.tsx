12ddf74a91d58df34167a5161429dd051fa1772a
Charles Borges <solucao.erp@gmail.com>
2024-02-25 18:14:30 -0300

    Frontend-Web: Refatoração de Redirecionamento de página para corrigir um fluxo indesejado

--- dscommerce-frontend/src/components/LoggedUser/index.tsx
+++ dscommerce-frontend/src/components/LoggedUser/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as authService from '../../services/auth-service';
 import { useContext } from 'react';
 import { ContextToken } from '../../utils/ContextToken';
@@ -6,12 +6,10 @@ import { ContextToken } from '../../utils/ContextToken';
 export default function LoggedUser() {
 
     const { contextTokenPayload, setContextTokenPayload } = useContext(ContextToken);
-    const navigate = useNavigate();
 
     function handleLogoutClick() {
         authService.logout();
         setContextTokenPayload(undefined);
-        navigate("/product-catalog");
     }
 
     return (
