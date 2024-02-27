35cce15b3070f58b74184b4c2ad0185b0852601b
Charles Borges <solucao.erp@gmail.com>
2024-02-27 19:54:32 -0300

    Frontend-Web: [form] definição das rotas de listagem e form-crud de produtos

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,25 +1,32 @@
-import { /*BrowserRouter*/ Navigate, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { unstable_HistoryRouter as HistoryRouter, Navigate, Route, Routes } from "react-router-dom";
 
+// Admin Routes
+import Admin from "./routes/Admin";
+import AdminHome from "./routes/Admin/Home";
+import ProductListing from "./routes/Admin/ProductListing";
+import ProductForm from './routes/Admin/ProductForm';
+
+// Client Routes
 import Client from "./routes/Client";
 import Login from "./routes/Client/Login";
 import ProductCatalog from "./routes/Client/ProductCatalog";
 import ProductDetails from "./routes/Client/ProductDetails";
 import Cart from "./routes/Client/Cart";
+import ConfirmationCart from "./routes/Client/ConfirmationCart";
+
+// Models
+import { AccessTokenPayloadDTO } from "./models/auth";
+
+// Services
 import * as authService from './services/auth-service';
 import * as cartService from './services/cart-service';
 
-import { useEffect, useState } from 'react';
-import { ContextCartCount } from "./utils/ContextCart";
-import Admin from "./routes/Admin";
-import AdminHome from "./routes/Admin/Home";
-
-// history
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+// History
 import { history } from './utils/history';
 import { PrivateRoute } from "./components/PrivateRoute";
-import { AccessTokenPayloadDTO } from "./models/auth";
+import { ContextCartCount } from "./utils/ContextCart";
 import { ContextToken } from "./utils/ContextToken";
-import ConfirmationCart from "./routes/Client/ConfirmationCart";
 
 export default function App() {
 
@@ -52,7 +59,10 @@ export default function App() {
             </Route>
 
             <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}><Admin /></PrivateRoute>}>
-              <Route index element={<AdminHome />} />
+              <Route index element={<Navigate to="home" />} />
+              <Route path="home" element={<AdminHome />} />
+              <Route path="products" element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
 
             <Route path="*" element={<Navigate to="/" />} />
