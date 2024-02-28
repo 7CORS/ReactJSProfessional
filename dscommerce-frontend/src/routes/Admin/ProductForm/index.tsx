784f71af65810a13b3d8e8a8c74734b89b88c4b4
Charles Borges <solucao.erp@gmail.com>
2024-02-27 21:05:53 -0300

    Frontend-Web: [form] Layout da página de formulário crud estático de produto (admin)

--- dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
+++ dscommerce-frontend/src/routes/Admin/ProductForm/index.tsx
@@ -1,5 +1,47 @@
+import './styles.css';
+
 export default function ProductForm() {
     return (
-        <h1>Formulário de Produtos</h1>
+        <main>
+            <section id="dsc-product-form-section" className="dsc-container">
+
+                <div className="dsc-product-form-container">
+                    <form className="dsc-card dsc-form">
+
+                        <h2>Dados do Produto</h2>
+
+                        <div className="dsc-form-controls-container">
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Nome" />
+                            </div>
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Preço" />
+                            </div>
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Imagem" />
+                            </div>
+                            <div>
+                                <select className="dsc-select dsc-form-control" required>
+                                    <option value="" disabled selected>Categoria</option>
+                                    <option value="1">Categoria 1</option>
+                                    <option value="2">Categoria 2</option>
+                                    <option value="3">Categoria 3</option>
+                                </select>
+                            </div>
+                            <div>
+                                <textarea className="dsc-form-control dsc-textarea" placeholder="Descrição"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="dsc-product-form-buttons">
+                            <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                            <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
+                        </div>
+
+                    </form>
+                </div>
+
+            </section>
+        </main>
     );
 }
