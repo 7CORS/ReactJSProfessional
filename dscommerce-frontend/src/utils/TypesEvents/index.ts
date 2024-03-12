e8a6c1abff2bf9ac93ce9954ce7f02fa0a7f912d
Charles Borges <solucao.erp@gmail.com>
2024-03-12 20:59:38 -0300

    Frontend-Web: Refactor interface FormInputProps

--- dscommerce-frontend/src/utils/TypesEvents/index.ts
+++ dscommerce-frontend/src/utils/TypesEvents/index.ts
@@ -5,8 +5,9 @@ export type ChangeEventT = ChangeEvent<HTMLInputElement>;
 
 // Extende as props de input padrão com possíveis customizações
 export interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
-
     validation?: (value: string) => boolean;
-
+    invalid?: string; // Se espera usar como string, mas talvez boolean seria mais adequado?
+    dirty?: string; // Mesmo comentário acima, parece que deveria ser boolean
+    onTurnDirty?: (name: string) => void; // Garanta que este método é passado onde esses componentes são usados
     // ...adicione outras props customizadas conforme necessário aqui
 }
