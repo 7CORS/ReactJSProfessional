6d2e350a5366091a2422eabf32bdebd2526fc90c
Charles Borges <solucao.erp@gmail.com>
2024-03-12 19:22:05 -0300

    Frontend-Web: Variáveis de ambiente para o Deploy configuradas

--- dscommerce-frontend/src/utils/System/index.ts
+++ dscommerce-frontend/src/utils/System/index.ts
@@ -1,12 +1,12 @@
 // URL base do backend
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+
+// ID do cliente para autenticação OAuth2
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+
+// Segredo do cliente para autenticação OAuth2
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
 
 // Chave para o armazenamento do carrinho e token no localStorage
 export const CART_KEY = "com.devsuperior.dscommerce/Cart";
 export const TOKEN_KEY = "com.devsuperior.dscommerce/Token";
-
-// ID do cliente para autenticação OAuth2
-export const CLIENT_ID = "myclientid";
-
-// Segredo do cliente para autenticação OAuth2
-export const CLIENT_SECRET = "myclientsecret";
